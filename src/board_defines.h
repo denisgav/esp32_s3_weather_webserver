e8066a8598d94a3b9e55d33168827a5e5a27e78b
Denis Gavrysh <gavden@gmail.com>
2025-08-29 11:57:49 +0300

    Connect TFT display and SD card

--- src/board_defines.h
+++ src/board_defines.h
@@ -1,7 +1,7 @@
 #pragma once
 // SPI	        |MOSI	    |MISO	    |CLK	    |CS
-// HSPI (SPI 2)	|GPIO 11	|GPIO 13	|GPIO 12	|GPIO 10
-// VSPI (SPI 3)	|GPIO 35	|GPIO 37	|GPIO 36	|GPIO 39
+// HSPI (SPI 1)	|GPIO 11	|GPIO 13	|GPIO 12	|GPIO 10
+// FSPI (SPI 0)	|GPIO 35	|GPIO 37	|GPIO 36	|GPIO 39
 
 // SD pins
 // SD -> SPI
@@ -12,13 +12,45 @@
 // CMD -> DI
 // CLK -> SCLK
 
-// SD MMC pins
-const int SD_MMC_CLK = 36; // SCK OTG
-const int SD_MMC_CMD = 35; // MOSI OTG
-const int SD_MMC_D0  = 37; // MISO OTG
+//--------------------------
+// SPI buses
+//--------------------------
+const int HSPI_SS    = 10;
+const int HSPI_MOSI  = 11;
+const int HSPI_SCK   = 12;
+const int HSPI_MISO  = 13;
 
-// Used VSPI bus
-const int SD_SCK  = SD_MMC_CLK;
-const int SD_MISO = SD_MMC_D0;
-const int SD_MOSI = SD_MMC_CMD;
-const int SD_CS   = 39; /* D3 */
+const int FSPI_SS    = 39;
+const int FSPI_MOSI  = 35;
+const int FSPI_SCK   = 36;
+const int FSPI_MISO  = 37;
+//--------------------------
+
+//--------------------------
+// TFT Display
+//--------------------------
+const int TFT_SCL  = HSPI_SCK;  // SPI clock signal
+const int TFT_SDA  = HSPI_MOSI; // Serial data input pin sda
+const int TFT_RES  = 40; // Reset pin
+const int TFT_DC   = 41; // Data selection signal
+const int TFT_CS   = HSPI_SS; // LCD chip select signal, for SPI protocol
+const int TFT_BLK  = -1; //
+//--------------------------
+
+//--------------------------
+// SD SPI pins option
+//--------------------------
+const int SD_SCK  = FSPI_SCK;
+const int SD_MISO = FSPI_MISO;
+const int SD_MOSI = FSPI_MOSI;
+const int SD_CS   = FSPI_SS; /* D3 */
+//--------------------------
+
+//--------------------------
+// SD MMC pins option
+//--------------------------
+const int SD_MMC_CLK = FSPI_SCK;  // SCK OTG
+const int SD_MMC_CMD = FSPI_MOSI; // MOSI OTG
+const int SD_MMC_D0  = FSPI_MISO; // MISO OTG
+//--------------------------
+
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,124 @@
 
 #include "board_defines.h"
 
+#include <FS.h>
+#include <SD_MMC.h>
 
-void setup() {}
+//-------------------------------------------------
+// ST7735
+//-------------------------------------------------
 
-void loop() {}
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include "Adafruit_miniTFTWing.h"
+
+//-------------------------------------------------
+
+// uninitialized pointers to SPI objects
+// SPIClass fspi = SPIClass(FSPI);
+SPIClass hspi = SPIClass(HSPI);
+
+// Adafruit_miniTFTWing ss;
+Adafruit_ST7735 tft = Adafruit_ST7735(&hspi, TFT_CS, TFT_DC, TFT_RES);
+
+int init_sd_mmc();
+int init_tft();
+
+void setup(void)
+{
+    Serial.begin(115200);
+
+    //fspi.begin(FSPI_SCK, FSPI_MISO, FSPI_MOSI, FSPI_SS);
+    hspi.begin(HSPI_SCK, HSPI_MISO, HSPI_MOSI, HSPI_SS);
+
+    if(init_sd_mmc() != 0){
+        while(true){
+            Serial.print("SD MMC init failed!");
+            delay(1000);
+        }
+    }
+
+    if(init_tft() != 0){
+        while(true){
+            Serial.print("TFT init failed!");
+            delay(1000);
+        }
+    }
+
+    Serial.print("Hello!");
+
+    Serial.println("Initialized");
+}
+
+void loop()
+{
+    delay(500);
+}
+
+int init_sd_mmc()
+{
+    if (!SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0))
+    {
+        Serial.println("Pin change failed!");
+        return -1;
+    }
+    if (!SD_MMC.begin("/sdcard", true, false, BOARD_MAX_SDMMC_FREQ, 5))
+    {
+        Serial.println("Card Mount Failed");
+        return -1;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+
+    if (cardType == CARD_NONE)
+    {
+        Serial.println("No SD_MMC card attached");
+        return -1;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if (cardType == CARD_MMC)
+    {
+        Serial.println("MMC");
+    }
+    else if (cardType == CARD_SD)
+    {
+        Serial.println("SDSC");
+    }
+    else if (cardType == CARD_SDHC)
+    {
+        Serial.println("SDHC");
+    }
+    else
+    {
+        Serial.println("UNKNOWN");
+        return -1;
+    }
+
+    uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+    Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+    return 0;
+}
+
+int init_tft()
+{
+    //   if (!ss.begin()) {
+    //     Serial.println("seesaw init error!");
+    //     while(1);
+    //   }
+    //   else Serial.println("seesaw started");
+
+    //   ss.tftReset();
+    //   ss.setBacklight(0x0); //set the backlight fully on
+
+    // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
+    tft.initR(INITR_MINI160x80); // initialize a ST7735S chip, mini display
+
+    tft.setRotation(3);
+
+    tft.setCursor(0, 0);
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(0);
+    tft.println("Hello World!");
+    return 0;
+}
