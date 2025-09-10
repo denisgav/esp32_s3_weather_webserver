e5be6e2dd80f2fe55ec0d88a5e36245287c550a4
Denis Gavrysh <gavden@gmail.com>
2025-09-10 14:07:34 +0300

    Add JSON library

--- Firmware/src/weather_web_server.cpp
+++ Firmware/src/weather_web_server.cpp
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 #include "weather_web_server.h"
 
 weather_web_server ::weather_web_server(multisensor_wrapper &multisensor) : multisensor(multisensor),
@@ -51,42 +53,48 @@ String weather_web_server ::fetch_sensor_data() const
 {
     DateTime now = multisensor.get_rtc().get_sampled_datetime();
 
-    String res = "{";
+    JsonDocument doc;
+
+    // Sampled RTC date time
+    doc["year"] = String(now.year());
+    doc["month"] = String(now.month());
+    doc["day"] = String(now.day());
+    doc["dayOfTheWeek"] = String(now.dayOfTheWeek());
+    doc["hour"] = String(now.hour());
+    doc["minute"] = String(now.minute());
+    doc["second"] = String(now.second());
 
     // DS3231
-    res += "\"ds3231_temperature\": \"" + String(multisensor.get_rtc().get_sampled_temperature()) + "\"";
-    res += ",\"year\": \"" + String(now.year()) + "\"";
-    res += ",\"month\": \"" + String(now.month()) + "\"";
-    res += ",\"day\": \"" + String(now.day()) + "\"";
-    res += ",\"dayOfTheWeek\": \"" + String(now.dayOfTheWeek()) + "\"";
-    res += ",\"hour\": \"" + String(now.hour()) + "\"";
-    res += ",\"minute\": \"" + String(now.minute()) + "\"";
-    res += ",\"second\": \"" + String(now.second()) + "\"";
+    doc["ds3231_temperature"] = String(multisensor.get_rtc().get_sampled_temperature());
 
     // LM35
-    res += ",\"lm35_temperature\": \"" + String(multisensor.get_lm35().get_sampled_temperature()) + "\"";
+    doc["lm35_temperature"] = String(multisensor.get_lm35().get_sampled_temperature());
 
     // BME280
-    res += ",\"bme280_temperature\": \"" + String(multisensor.get_bme280().get_sampled_temperature()) + "\"";
-    res += ",\"bme280_pressure\": \"" + String(multisensor.get_bme280().get_sampled_pressure()) + "\"";
-    res += ",\"bme280_humidity\": \"" + String(multisensor.get_bme280().get_sampled_humidity()) + "\"";
+    doc["bme280_temperature"] = String(multisensor.get_bme280().get_sampled_temperature());
+    doc["bme280_pressure"] = String(multisensor.get_bme280().get_sampled_pressure());
+    doc["bme280_humidity"] = String(multisensor.get_bme280().get_sampled_humidity());
 
     // DHT11
-    res += ",\"dht11_temperature\": \"" + String(multisensor.get_dht11().get_sampled_temperature()) + "\"";
-    res += ",\"dht11_humidity\": \"" + String(multisensor.get_dht11().get_sampled_humidity()) + "\"";
+    doc["dht11_temperature"] = String(multisensor.get_dht11().get_sampled_temperature());
+    doc["dht11_humidity"] = String(multisensor.get_dht11().get_sampled_humidity());
 
     // ENS160
-    res += ",\"ens160_AQI\": \"" + String(multisensor.get_ens160().get_sampled_AQI()) + "\"";
-    res += ",\"ens160_TVOC\": \"" + String(multisensor.get_ens160().get_sampled_TVOC()) + "\"";
-    res += ",\"ens160_eCO2\": \"" + String(multisensor.get_ens160().get_sampled_eCO2()) + "\"";
+    doc["ens160_AQI"] = String(multisensor.get_ens160().get_sampled_AQI());
+    doc["ens160_TVOC"] = String(multisensor.get_ens160().get_sampled_TVOC());
+    doc["ens160_eCO2"] = String(multisensor.get_ens160().get_sampled_eCO2());
 
     // AHT2x
-    res += ",\"aht2x_temperature\": \"" + String(multisensor.get_aht2x().get_sampled_temperature()) + "\"";
-    res += ",\"aht2x_humidity\": \"" + String(multisensor.get_aht2x().get_sampled_humidity()) + "\"";
+    doc["aht2x_temperature"] = String(multisensor.get_aht2x().get_sampled_temperature());
+    doc["aht2x_humidity"] = String(multisensor.get_aht2x().get_sampled_humidity());
 
     // LDR
-    res += ",\"ldr\": \"" + String(multisensor.get_ldr().get_sampled_ldr()) + "\"";
+    doc["ldr"] = String(multisensor.get_ldr().get_sampled_ldr());
 
-    res += "}";
-    return res;
+    
+    // Lastly, you can print the resulting JSON to a String
+    String output;
+    serializeJson(doc, output);
+
+    return output;
 }
